3194544217801e62beb635c4a2eded0c825ef88e
Pacôme Françon <pacome.francon@epitech.eu>
2018-11-30 10:09:27 +0100

    add comment memcpy

--- src/memcpy.s
+++ src/memcpy.s
@@ -2,16 +2,16 @@
 	section	.text
 
 memcpy:
-	mov	rax, rdi
-	cmp	rdx, 0
-	jz	.EXIT
+	mov	rax, rdi	; return a pointer to dest (rdi)
+	cmp	rdx, 0		; if (n == 0)
+	jz	.EXIT		; don't try to copy anything
 
 .LOOP:
-	dec	rdx
-	mov	r8b, BYTE[rsi + rdx]
-	mov	BYTE[rdi + rdx], r8b
-	cmp	rdx, 0
-	jnz	.LOOP
+	dec	rdx		; third parameter use as index
+	mov	r8b, BYTE[rsi + rdx] ; r8b = src[n]
+	mov	BYTE[rdi + rdx], r8b ; dest[n] = r8b => copy
+	cmp	rdx, 0		     ; if (n != 0)
+	jnz	.LOOP		     ; loop
 
 .EXIT:
 	ret
