edac4a1b0069df561c67b687b7e492a1626ebe48
Pacôme Françon <pacome.francon@epitech.eu>
2019-01-02 15:22:13 +0100

    add comment strcasecmp

--- src/strcasecmp.s
+++ src/strcasecmp.s
@@ -1,7 +1,7 @@
 	global	strcasecmp:function
 	section	.text
 
-	;; int int strcasecmp(const char *s1, const char *s2)
+	;; int strcasecmp(const char *s1, const char *s2)
 
 strcasecmp:
 	xor	rax, rax
@@ -18,28 +18,28 @@ strcasecmp:
 	add	r8b, 0x20	; "else" 0x20 -> ' ' -> ('a' - 'A')
 
 .ARGV_2:
-	cmp	r9b, 0x41
-	jb	.STRCMP
-	cmp	r9b, 0x5A
-	ja	.STRCMP
-	add	r9b, 0x20
+	cmp	r9b, 0x41	; if (*s2 < 'A')
+	jb	.STRCMP		; jump to compare
+	cmp	r9b, 0x5A	; if (*s2 > 'Z')
+	ja	.STRCMP		; jump to compare
+	add	r9b, 0x20	; "else" 0x20 -> ' ' -> ('a' - 'A')
 
 .STRCMP:
-	mov	al, r8b
-	cmp	r9b, al
-	jnz	.DIFF
-	cmp	r8b, 0
-	jz	.EXIT
-	inc	rsi
-	inc	rdi
-	jmp	.LOOP
+	mov	al, r8b		; al is 8 bytes part of rax : al = *s1
+	cmp	r9b, al		; if (*s2 != *s1)
+	jnz	.DIFF		; return *s2 - *s1
+	cmp	r8b, 0		; if (*s1 == '\0')
+	jz	.EXIT		; return 0
+	inc	rdi		; s1++
+	inc	rsi		; s2++
+	jmp	.LOOP		; loop
 
 .DIFF:
-	xor	rcx, rcx
-	mov	cl, r9b
-	sub	eax, ecx
-	ret
+	xor	rcx, rcx	; init tmp to 0
+	mov	cl, r9b		; cl is 8 bytes part of rcx : cl = *s2
+	sub	eax, ecx	; eax (return value) = eax -= *s2
+	ret			; return *s2 - *s1
 
 .EXIT:
-	xor	rax, rax
+	xor	rax, rax	; return 0
 	ret
