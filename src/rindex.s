5f2e6b0d1bb3ac36bfd470b0de03d5d76f46d731
Pacôme Françon <pacome.francon@epitech.eu>
2019-02-11 18:48:28 +0100

    fix rindex.s + add rindex tests

--- src/rindex.s
+++ src/rindex.s
@@ -1,33 +1,19 @@
 	global	rindex:function
 	section	.text
 
-	;; int putchar(int c)
+	;; char *rindex(const char *s, int c)
 
 rindex:
-	mov	rax, rdi	; mov first parameter to rax (return value)
-
-	;; rindex is like strchr, but start search at the end of the string
-	;; so we use strlen to go until null-terminated byte
-
-.STRLEN:
-	cmp	BYTE[rax], 0	; cmp each char with 8-bit part of rsi (second parameter)
-	jz	.START		; jump when end of string is reached
-	inc	rax		; inc index
-	jmp	.STRLEN		; strlen loop
-
-.START:
-	dec	rax		; rax == strlen(), so we use rax as index, so len - 1
+	xor	eax, eax	; init rax to NULL because not found return NULL
 
 .LOOP:
-	cmp	BYTE[rax], 0	; cmp each char with '\0'
-	jz	.EXIT		; end of loop, return NULL => not found
-	cmp	BYTE[rax], sil:	; cmp each char with 8-bit part of rsi (second parameter)
-	jz	.FIND		; end  of loop, found
-	dec	rax		; dec index, because loop is reversed
-	jmp	.LOOP		; loop
+	mov	cl, BYTE[rdi]	; cl = *s (cl is 8-bit part of rcx, use as tmp)
+	cmp	cl, sil		; sil is the searched byte
+	cmove	rax, rdi	; if (*s == c) => save address
 
-.EXIT:
-	xor	rax, rax	; return NULL
+	;; cmove is performed if condition flag is set
 
-.FIND:
-	ret
+	inc	rdi		; s++
+	cmp	cl, 0		; if (*(s - 1) == '\0')
+	jne	.LOOP		; loop
+	ret			; return
--- tests/src/tests-my_rindex.c
+++ tests/src/tests-my_rindex.c
@@ -42,23 +42,57 @@ Test(utils, rindex_not_found, .init = init, .fini = fini)
 {
 	str = strdup("bonjour");
 	assert(str);
+	ret_sys = rindex(str, 'z');
 	my_ret = (*my_rindex)(str, 'z');
 	cr_assert(my_ret == NULL);
+	cr_assert(ret_sys == NULL);
+	cr_assert(my_ret == ret_sys);
 }
 
-Test(utils, rindex_empty_string, .init = init, .fini = fini)
-{
-	str = strdup("");
-	assert(str);
-	my_ret = (*my_rindex)(str, 'z');
-	cr_assert(my_ret == NULL);
-}
-
-Test(utils, rindex_one_char, .init = init, .fini = fini)
+Test(utils, rindex_one_char_string_length_1, .init = init, .fini = fini)
 {
 	str = strdup("z");
 	assert(str);
-	my_ret = (*my_rindex)(str, 'z');
 	ret_sys = rindex(str, 'z');
+	my_ret = (*my_rindex)(str, 'z');
+	cr_assert(my_ret != NULL);
+	cr_assert(ret_sys != NULL);
+	cr_assert(my_ret == ret_sys);
 	cr_assert(strcmp(ret_sys, my_ret) == 0);
+}
+
+Test(utils, rindex_one_char_empty_string, .init = init, .fini = fini)
+{
+	str = strdup("");
+	assert(str);
+	ret_sys = rindex(str, 'z');
+	my_ret = (*my_rindex)(str, 'z');
+	cr_assert(my_ret == NULL);
+	cr_assert(ret_sys == NULL);
+	cr_assert(my_ret == ret_sys);
+}
+
+Test(utils, rindex_0_empty_string, .init = init, .fini = fini)
+{
+	str = strdup("");
+	assert(str);
+	my_ret = (*my_rindex)(str, '\0');
+	ret_sys = rindex(str, '\0');
+	cr_assert(my_ret != NULL);
+	cr_assert(ret_sys != NULL);
+	cr_assert(my_ret == ret_sys);
+	cr_assert(strcmp(ret_sys, my_ret) == 0);
+}
+
+Test(utils, rindex_0_string, .init = init, .fini = fini)
+{
+	str = strdup("zmerefrefref");
+	assert(str);
+	my_ret = (*my_rindex)(str, '\0');
+	ret_sys = rindex(str, '\0');
+	cr_assert(my_ret != NULL);
+	cr_assert(ret_sys != NULL);
+	cr_assert(*my_ret == '\0');
+	cr_assert(*ret_sys == '\0');
+	cr_assert(my_ret == ret_sys);
 }
