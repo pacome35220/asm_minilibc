a88ce54de4b16d9960bf003417d30e3f971ae19f
Pacôme Françon <pacome.francon@epitech.eu>
2019-03-01 16:15:28 +0100

    add some comments

--- src/memmove.s
+++ src/memmove.s
@@ -4,28 +4,28 @@
 	;; void *memmove(void *dest, const void *src, size_t n)
 
 memmove:
-	cmp	rsi, rdi
-	jb	.MEMCPY
-	mov     rax, rdi
-	mov     rcx, rdx
-	mov     rdx, rsi
-	cld
-	rep     movsb
-	mov     rsi, rdx
-	mov     rdi, rax
-	ret
+	cmp	rsi, rdi	; if (src < dest)
+	jl	.MEMCPY		; jump to basic memcpy
+	mov	rax, rdi	; rax = dest
+	mov	rcx, rdx	; rcx = n
+	mov	rdx, rsi	; rdx = src
+	cld			; clear direction flag
+	rep	movsb		; magic
+	mov	rsi, rdx	; restore src
+	mov	rdi, rax	; restore dest
+	ret			; return dest
 
 .MEMCPY:
-	mov	rax, rdi
-	cmp	rdx, 0
-	jz	.EXIT
+	mov	rax, rdi	; return a pointer to dest (rdi)
+	cmp	rdx, 0		; if (n == 0)
+	jz	.EXIT		; don't try to copy anything
 
 .LOOP:				; memcpy loop
-	dec	rdx
-	mov	r8b, BYTE[rsi + rdx]
-	mov	BYTE[rdi + rdx], r8b
-	cmp	rdx, 0
-	jnz	.LOOP
+	dec	rdx		; third parameter use as index
+	mov	r8b, BYTE[rsi + rdx] ; r8b = src[n]
+	mov	BYTE[rdi + rdx], r8b ; dest[n] = r8b => copy
+	cmp	rdx, 0		     ; if (n != 0)
+	jnz	.LOOP		     ; loop
 
 .EXIT:
 	ret
--- src/strpbrk.s
+++ src/strpbrk.s
@@ -4,28 +4,28 @@
 	;; char *strpbrk(const char *s, const char *accept)
 
 strpbrk:
-	mov	rax, rdi
+	mov	rax, rdi	; mov first parameter to rax (return value)
 
 .LOOP:
-	cmp	BYTE[rax], 0
-	jz	.EXIT
+	cmp	BYTE[rax], 0	; if (*s == '\0')
+	jz	.EXIT		; return NULL
 	xor	rcx, rcx	; rcx use as index
 
 .CMP:
-	mov	r8b, BYTE[rsi + rcx]
+	mov	r8b, BYTE[rsi + rcx] ; r8b -> tmp = accept[rcx]
 	cmp	r8b, 0		; while (rsi[rcx] != 0)
-	jz	.END_WHILE
-	cmp	BYTE[rax], r8b	; if (*s == rsi[rcx])
-	jz	.FIND
+	jz	.END_WHILE	; jump to rcx++
+	cmp	BYTE[rax], r8b	; if (*s == accept[rcx])
+	jz	.FIND		; return rax
 	inc	rcx		; rcx++
-	jmp	.CMP
+	jmp	.CMP		; loop accept[rcx]
 
 .END_WHILE:
-	inc	rax
-	jmp	.LOOP
+	inc	rax		; rax++
+	jmp	.LOOP		; main loop
 
 .EXIT:
-	xor	rax, rax
+	xor	rax, rax	; return 0
 
 .FIND:
-	ret
+	ret			; return ptr
